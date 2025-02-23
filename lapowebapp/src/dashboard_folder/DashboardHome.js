cdaf6d9f82a14d4790c4182fc23dc8d39d642deb
blessingayo <asuelimenblessing630@gmail.com>
2025-02-23 16:25:34 +0100

    dashboard completed

--- lapowebapp/src/dashboard_folder/DashboardHome.js
+++ lapowebapp/src/dashboard_folder/DashboardHome.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/DashboardHome.css";
 import { CiCreditCard2 } from "react-icons/ci";
 
@@ -6,11 +6,13 @@ import { FaChevronRight } from "react-icons/fa6";
 import { CiCreditCard1 } from "react-icons/ci";
 import { Divider } from "@mui/material";
 import { MdAddCard } from "react-icons/md";
-import { BarChart } from "@mui/x-charts/BarChart";
 import Table from "../assetsFolder/svg/Table.svg";
 import lineChart from "../assetsFolder/svg/lineChart.svg";
 import pie from "../assetsFolder/svg/pie.svg";
 import Monthly from "../assetsFolder/svg/Monthly.svg";
+import { MdOutlineArrowOutward } from "react-icons/md";
+import { LiaMoneyBillWaveSolid, LiaHourglassHalfSolid } from "react-icons/lia";
+import { IoAlertCircleOutline } from "react-icons/io5";
 
 const firstCardSection = [
   {
@@ -69,57 +71,174 @@ const DashboardHome = () => {
 
         <div className="sec">
           <div className="secondSec">
-            <div className="">
-              <CiCreditCard1 />
+            <div style={{ width: "90%", margin: "0 auto" }}>
+              <CiCreditCard1
+                style={{
+                  color: "#00984C",
+                  marginBottom: "5px",
+                  marginTop: "3px",
+                }}
+              />
+
+              <p style={{ marginTop: "-2px" }}>Total Active Cards</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p
+                  style={{
+                    fontWeight: "700",
+                    fontSize: "24px",
+                    marginTop: "-0px",
+                  }}
+                >
+                  26,478
+                </p>
+                <div
+                  style={{
+                    background: "#EFFAF6",
+                    width: "56px",
+                    height: "20px",
+                    color: "#29A174",
+                    fontSize: "10px",
+                    textAlign: "center",
+                    paddingTop: "3px",
+                    marginTop: "10px",
+                  }}
+                >
+                  <MdOutlineArrowOutward /> +90%
+                </div>
+                <div style={{ marginTop: "6px" }}>this month</div>
+              </div>
             </div>
-            <p>Total Active Cards</p>
-            <h3>26,478</h3>
           </div>
           <div className="secondSec">
-            <div className="">
-              <CiCreditCard1 />
+            <div style={{ width: "90%", margin: "0 auto" }}>
+              <CiCreditCard2
+                style={{
+                  color: "#8020E7",
+                  marginBottom: "5px",
+                  marginTop: "3px",
+                }}
+              />
+
+              <p style={{ marginTop: "-2px" }}>Total Personalized Cards</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p
+                  style={{
+                    fontWeight: "700",
+                    fontSize: "24px",
+                    marginTop: "-0px",
+                  }}
+                >
+                  15,703
+                </p>
+                <div
+                  style={{
+                    background: "#EFFAF6",
+                    width: "56px",
+                    height: "20px",
+                    color: "#29A174",
+                    fontSize: "10px",
+                    textAlign: "center",
+                    paddingTop: "3px",
+                    marginTop: "10px",
+                  }}
+                >
+                  <MdOutlineArrowOutward /> 8.5%
+                </div>
+                <div style={{ marginTop: "6px" }}>this month</div>
+              </div>
             </div>
-            <p>Total Active Cards</p>
-            <h3>26,478</h3>
-          </div>{" "}
+          </div>
           <div className="secondSec">
-            <div className="">
-              <CiCreditCard1 />
+            <div style={{ width: "90%", margin: "0 auto" }}>
+              <LiaMoneyBillWaveSolid
+                style={{
+                  color: "#2087E7",
+                  marginBottom: "5px",
+                  marginTop: "3px",
+                }}
+              />
+
+              <p style={{ marginTop: "-2px" }}>Today’s Revenue</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p
+                  style={{
+                    fontWeight: "700",
+                    fontSize: "24px",
+                    marginTop: "-0px",
+                  }}
+                >
+                  ₦9.3M
+                </p>
+                <div
+                  style={{
+                    background: "#EFFAF6",
+                    width: "56px",
+                    height: "20px",
+                    color: "#29A174",
+                    fontSize: "10px",
+                    textAlign: "center",
+                    paddingTop: "3px",
+                    marginTop: "10px",
+                  }}
+                >
+                  <MdOutlineArrowOutward /> +6%
+                </div>
+                <div style={{ marginTop: "6px" }}>yesterday</div>
+              </div>
             </div>
-            <p>Total Active Cards</p>
-            <h3>26,478</h3>
-          </div>{" "}
+          </div>
           <div className="secondSec">
-            <div className="">
-              <CiCreditCard1 />
+            <div style={{ width: "90%", margin: "0 auto" }}>
+              <LiaHourglassHalfSolid
+                style={{
+                  color: "#E78020",
+                  marginBottom: "5px",
+                  marginTop: "3px",
+                }}
+              />
+
+              <p style={{ marginTop: "-2px" }}>Pending Requests</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p
+                  style={{
+                    fontWeight: "700",
+                    fontSize: "24px",
+                    marginTop: "-0px",
+                  }}
+                >
+                  38
+                </p>
+                {/* <div
+                  style={{
+                    background: "#EFFAF6",
+                    width: "56px",
+                    height: "20px",
+                    color: "#29A174",
+                    fontSize: "10px",
+                    textAlign: "center",
+                    paddingTop: "3px",
+                    marginTop: "10px",
+                  }}
+                >
+                  <MdOutlineArrowOutward /> +90%
+                </div> */}
+                <div style={{ marginTop: "6px", color: "#E78020" }}>
+                  <IoAlertCircleOutline />
+                  Requires attention
+                </div>
+              </div>
             </div>
-            <p>Total Active Cards</p>
-            <h3>26,478</h3>
           </div>
         </div>
-        {/* 
-        <div className="third-sec">
-          <div>
-            <img style={{ width: "99%" }} src={Monthly} alt="" />
-          </div>
-
-          <div className="table">
-            <img src={Table} alt="Table" />
-          </div>
-        </div>
-
-        <div className="fourth-sec">
-          <div style={{ height: "260px" }}>
-            <img src={lineChart} alt="line-chart" />
-          </div>
-          <div style={{ marginTop: "-2rem" }}>
-            <img src={pie} alt="pie" />
-          </div>
-        </div> */}
 
         <div className="third-sec">
           <div className="chart-container">
-            <img className="full-width-img" src={Monthly} alt="Monthly Chart" />
+            <img
+              className="full-width-img"
+              style={{ marginTop: "-2px" }}
+              src={Monthly}
+              alt="Monthly Chart"
+            />
           </div>
 
           <div className="table-container">
@@ -129,7 +248,7 @@ const DashboardHome = () => {
 
         <div className="fourth-sec">
           <div className="chart-box">
-            <img className="chart-img" src={lineChart} alt="Line Chart" />
+            <img className="line-img" src={lineChart} alt="Line Chart" />
           </div>
           <div className="chart-box">
             <img className="chart-img" src={pie} alt="Pie Chart" />
